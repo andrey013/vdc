b7cf238325bcfde871fb4b32459b186a02fd8e0a
Andrey Yankin <yankin013@gmail.com>
2012-10-01 21:57:38 +0400

    main navbar

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,3 +1,10 @@
+<?php                                                                                                                                   
+$length = ob_get_length();
+$filename = '/home/andrey/yii/vdc/protected/views/layouts/main.php';
+$last_modified = date ("F d Y H:i:s", filemtime($filename));//, getlastmod());                                                                                    
+header("Content-Length: $length");                                                                                                      
+header("Last-Modified: $last_modified GMT time");                                                                                       
+?>
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -5,55 +12,80 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.min.css" />
+
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.min.js"></script>
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.min.js"></script>
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/live.js"></script>
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
+<script>
+$(function(){
+	$('.dropdown-toggle').dropdown();
+});
+</script>
+
 <body>
 
-<div class="container" id="page">
+	<div class="navbar navbar-static-top">
+		<div class="navbar-inner">
+			<a class="brand" href="#">ВДЦ</a>
+			<ul class="nav">
+				<li class="active"><a href="<?php echo $this->createUrl('/site/index'); ?>">Заказы</a></li>
+				<li><a href="<?php echo $this->createUrl('/site/page', array('view'=>'about')); ?>">Настройки</a></li>
+				<li><a href="<?php echo $this->createUrl('/site/contact'); ?>">Дизайнеры</a></li>
+				<li><a href="<?php echo $this->createUrl('/site/login'); ?>">Пользователи</a></li>
+				<li class="dropdown">
+    				<a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" href="#">
+    					Скачать <b class="caret"></b>
+    				</a>
+    				<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+    					<li><a href="#"><i class="icon-file"></i> Инструкция</a></li>
+    					<li><a href="#"><i class="icon-file"></i> Прайс</a></li>
+    					<li><a href="#"><i class="icon-file"></i> Описание принципа работы ЕДЦ</a></li>
+    					<li><a href="#"><i class="icon-plus"></i> Создать ссылку на документ</a></li>
+    				</ul>
+    			</li>
+				<li><a href="<?php echo $this->createUrl('/site/page', array('view'=>'about')); ?>">Контакты ВДЦ</a></li>
+			</ul>
+			<ul class="nav pull-right">
+				
+				<li class="dropdown">
+					<div class="btn-group">
+    				<a class="btn btn-success dropdown-toggle" id="status" role="button" data-toggle="dropdown" href="#">
+    					Виктория К. <i class="icon-ok"></i> Свободен <b class="caret"></b>
+    				</a>
+    				<ul class="dropdown-menu" role="menu" aria-labelledby="status">
+    					<li><a href="#"><i class="icon-ok"></i> Свободен</a></li>
+    					<li><a href="#"><i class="icon-off"></i> Занят</a></li>
+    				</ul>
+    				</div>
+    			</li>
+				<?php if(Yii::app()->user->isGuest){ ?>
+				<li><a href="<?php echo $this->createUrl('/site/login'); ?>">Login</a></li>
+				<?php }else{ ?>
+				<li><a href="<?php echo $this->createUrl('/site/logout'); ?>">
+					<?php echo 'Logout ('.Yii::app()->user->name.')' ?>
+					</a>
+				</li>
+				<?php } ?>
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
+			</ul>
+		</div>
+	</div>
+	
 	<?php echo $content; ?>
 
-	<div class="clear"></div>
+	<div class="clearfix"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		Copyright &copy; <?php echo date('Y'); ?> Всё для Вас<br/>
+		All Rights Reserved.
 	</div><!-- footer -->
-
-</div><!-- page -->
 
 </body>
 </html>
